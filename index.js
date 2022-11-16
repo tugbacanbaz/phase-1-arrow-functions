ebf72121d530eabfa8611c8585b129477499601f
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-11-17 00:58:24 +0300

    Complited Assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(){
+    let divided=2000/100;
+    return divided;
+}
+
+const square=(x)=>{
+    let squared=x*x;
+    return squared;
+}
+
+const add=(a,b)=>{
+    let added=a+b;
+    return added;
+}
